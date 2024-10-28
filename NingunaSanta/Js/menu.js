228818ed4469fb09c4ff4a51f9825858bd31430c
Alejo Jimenez <alejoyuber@gmail.com>
2024-10-28 18:32:04 -0300

    agrego archivos JS y otras secuencias html

---
+++ NingunaSanta/Js/menu.js
@@ -0,0 +1,15 @@
+const txtPuntaje = document.querySelector("#puntos");
+const nombre = document.querySelector("#nombre");
+const nombreJugador = document.querySelector("#nombre-jugador");
+
+nombre.innerHTML = localStorage.getItem("nombre");
+nombreJugador.innerHTML = localStorage.getItem("nombre");
+
+let puntajeTotal = 0;
+if(!localStorage.getItem("puntaje-total")){
+    puntajeTotal = 0;
+    txtPuntaje.innerHTML = puntajeTotal
+}else{
+    puntajeTotal = parseInt(localStorage.getItem("puntaje-total"));
+    txtPuntaje.innerHTML = puntajeTotal;
+}
